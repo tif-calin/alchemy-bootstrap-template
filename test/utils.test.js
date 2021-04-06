3d240e0947ccfe5808c176511d81025c9e90e69b
tif-calin <culitif@tuta.io>
2021-04-05 20:01:56 -0700

    modify tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,18 +1,14 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+// import { example } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
+test('time to test a function', expect => {
     // Set up your arguments and expectations
     const expected = true;
     
-    //Act 
     // Call the function you're testing and set the result to a const
     const actual = true;
 
-    //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
