b0f4261eb20fb8d0a3a2679bd741fe36802dca41
tif-calin <culitif@tuta.io>
2021-04-08 12:27:10 -0700

    rename var in findByProperty

--- utils.js
+++ utils.js
@@ -1,14 +1,14 @@
 
 export function findByProperty(arr, val, key = 'id', all = false) {
     // will return either the first instance or all instances of items in an array who have a property with a certain value
-    let ret = [];
+    let items = [];
 
     for (let item of arr) {
         if (item[key] === val) {
             if (!all) return item;
-            ret.push(item);
+            items.push(item);
         };
     };
 
-    return ret;
+    return items;
 };
